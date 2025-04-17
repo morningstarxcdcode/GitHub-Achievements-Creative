7dde16681c5d12f3d6398bb2e14947dbdb920ea8
morningstarxcd <sourav.xcd@gmail.com>
2025-04-17 11:01:06 +0530

    Add unique Python function for Feature 1

--- python/example.py
+++ python/example.py
@@ -1,6 +1,6 @@
-# Unique Python Example
-def unique_function():
-    return "This is a unique Python function!"
+# Unique Python Function for Feature 1
+def unique_function_1():
+    return "This is a unique Python function for Feature 1!"
 
 if __name__ == "__main__":
-    print(unique_function())
+    print(unique_function_1())
